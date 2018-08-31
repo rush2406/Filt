0a0f5674b99e702cb40fe4bda3eee83b715d73e8
Nilesh Sinha <nileshsinha707@gmail.com>
2018-08-31 22:23:35 +0530

    Merge branch 'master' of https://github.com/parasjain1/tz18Android into feature_nilesh
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_event_map_list.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/org/technozion/technozion18/EventLandingPage.java
+++ app/src/main/java/org/technozion/technozion18/EventLandingPage.java
@@ -2,6 +2,8 @@ package org.technozion.technozion18;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.technozion.technozion18.common.OnEntityReceivedListener;
@@ -20,12 +22,31 @@ public class EventLandingPage extends AppCompatActivity {
 
         if(getIntent().getIntExtra("eventId", 0) != 0) {
             eventId = getIntent().getIntExtra("eventId", 0);
+            final String TAG="EventLandingPage";
+            eventId = getIntent().getIntExtra("eventId", 0);
+            final ImageView event_image=findViewById(R.id.event_image);
+            final TextView curved_data=findViewById(R.id.curve_shape_data);
+            final TextView event_main_text=findViewById(R.id.event_main_text);
+            final TextView event_small_desc=findViewById(R.id.event_small_desc);
+            final TextView calender_date=findViewById(R.id.calender_date);
+            final TextView clock_data=findViewById(R.id.clock_data);
+            final ImageView share_data=findViewById(R.id.share_data);
+            final EditText description_data=findViewById(R.id.description_data);
+            final EditText entry_charge=findViewById(R.id.entry_charge);
+            final EditText address_text=findViewById(R.id.address_text);
+            final EditText terms_text=findViewById(R.id.terms_text);
             final TextView textView = findViewById(R.id.textView);
             new EventPresenter().getEvent(eventId, new OnEntityReceivedListener<Event>(this) {
                 @Override
                 public void onReceived(Event event) {
                     EventLandingPage.this.event = event;
-                    textView.setText(event.getName());
+                    event_main_text.setText(event.getName());
+                    curved_data.setText((event.getEvent_type()));
+                    calender_date.setText(event.getEvent_time());
+                    description_data.setText(event.getEvent_type());
+                    event_small_desc.setText(event.getAlias());
+                    description_data.setEnabled(false);
+
                 }
             });
         }
